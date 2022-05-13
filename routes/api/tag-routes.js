6ef2c71e0e6e01bdc4f744469f0a26342f9fd316
Fisher Davis <fisherd02@yahoo.com>
2022-05-13 00:45:15 -0400

    got the routes working and the app functions

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,58 +1,61 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll({include: [{model:Product, through: ProductTag}]})
-  .then((tags) => res.json(tags))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+  Tag.findAll({ include: [{ model: Product, through: ProductTag }] })
+    .then((tags) => res.json(tags))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  Tag.findOne({where: {id: req.params.id}, include: [{model: Product, through: ProductTag}]})
-  .then((tag) => res.json(tag))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+  Tag.findOne({
+    where: { id: req.params.id },
+    include: [{ model: Product, through: ProductTag }],
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
   Tag.create(req.body)
-  .then((tag) => res.status(200).json(tag))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update({where: {id:req.params.id}})
-  .then((tag) => res.status(200).json(tag))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+  Tag.update({ where: { id: req.params.id } })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
-  Tag.destroy({where: {id:req.params.id}})
-  .then((tag) => res.status(200).json(tag))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+  Tag.destroy({ where: { id: req.params.id } })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const seeds = require('./seeds')
-// import sequelize connection
+const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,10 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-app.use(seeds)
+
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
+
